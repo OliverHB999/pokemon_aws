2e342af5c101085bd67aec748ffbaec38fcb0642
Oliver <oliver.barasinski@gmail.com>
2023-01-25 14:52:45 +0100

    big pushy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
+  const [searchPoke, setsearchPoke] = useState(null);
+  const [pokemons, setpokemons] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [loading2, setloading2] = useState(false);
+  const [loading3, setloading3] = useState(false);
+
+  const [addRes, setaddRes] = useState("");
+
+  const searchAction = () => {
+    setloading(true);
+
+    const name = document.querySelector("#search_in").value;
+    let url2 = `https://kntqz3vgpivqtc4iekiykp7lee0bmruu.lambda-url.eu-west-1.on.aws/api/pokemon/${name}`;
+
+    fetch(url2, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => setsearchPoke(res))
+      .finally(() => setloading(false));
+  };
+
+  const addAction = () => {
+    setloading2(true);
+
+    const name = document.querySelector("#add_in").value;
+    let url2 = `https://kntqz3vgpivqtc4iekiykp7lee0bmruu.lambda-url.eu-west-1.on.aws/api/pokemon/postToS3/${name}/pokemons-223311`;
+
+    fetch(url2, {
+      method: "POST",
+    })
+      .then((res) => setaddRes(res))
+      .finally(() => {
+        setloading2(false);
+      });
+  };
+
+  const loadAction = () => {
+    setloading3(true);
+
+    let url2 = `https://kntqz3vgpivqtc4iekiykp7lee0bmruu.lambda-url.eu-west-1.on.aws/api/pokemon/AllPokemons`;
+
+    fetch(url2, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => setpokemons(res))
+      .finally(() => setloading3(false));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="search_div">
+          <h3>search for a pokemon</h3>
+          <input id="search_in" type="text" placeholder="name" />
+          <button
+            id="search_btn_in"
+            onClick={() => {
+              searchAction();
+            }}
+          >
+            search
+          </button>
+          <ClipLoader loading={loading} color="green"></ClipLoader>
+          {searchPoke ? (
+            <div className="search_result">
+              <h5 style={{ margin: 0 }}>{searchPoke.Name}</h5>
+              <h6>type: {searchPoke.Type}</h6>{" "}
+              <p style={{ fontWeight: 700 }}>Abilities:</p>
+              {searchPoke.Abilities.map((ele) => {
+                return <p key={ele}>{ele}</p>;
+              })}
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <div className="search_div">
+          <h3>add a pokemon</h3>
+          <input id="add_in" type="text" placeholder="name" />
+          <button
+            id="search_btn_in"
+            onClick={() => {
+              addAction();
+            }}
+          >
+            add
+          </button>
+          <ClipLoader loading={loading2} color="green"></ClipLoader>
+          {addRes.status === 200 ? (
+            <p>pokemon added to S3</p>
+          ) : (
+            <p>
+              {addRes.status ? addRes.status + ":something went wrong" : ""}
+            </p>
+          )}
+        </div>
+
+        <div className="search_div">
+          <h3>your pokemons</h3>
+          <button
+            id="load_btn"
+            onClick={() => {
+              loadAction();
+            }}
+          >
+            load
+          </button>
+          <ClipLoader loading={loading3} color="green"></ClipLoader>
+          {pokemons.length > 0 ? (
+            <div className="search_result">
+              {pokemons.map((ele) => {
+                return <p key={ele}>{ele}</p>;
+              })}
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
       </header>
     </div>
   );
